9615e714c89e8260b3e70619a4f2f66a44be4538
Torcetta Alessandro Orazio Fausto <a.torcetta@reply.it>
2021-11-09 17:59:06 +0100

    Visitarefs #authorization - inizio add visita

--- src/components/AddVisita.js
+++ src/components/AddVisita.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import VisitaDataService from "../services/VisitaService";
 
+import AuthService from "../services/auth.service";
+
 const AddVisita = () => {
   const initialVisitaState = {
     id: null,
     luogo: "",
     data_avvio: "",
-    data_scadenza: ""
+    data_scadenza: "",
+    nomePaziente: "",
+    cognomePaziente: "",
+    userid: null
   };
   const [visita, setVisita] = useState(initialVisitaState);
   const [submitted, setSubmitted] = useState(false);
+
+  const user = AuthService.getCurrentUser();
 
   const handleInputChange = event => {
     const { name, value } = event.target;
@@ -17,11 +24,17 @@ const AddVisita = () => {
   };
 
   const saveVisita = () => {
-    var data = {
-      luogo: visita.luogo,
-      data_avvio: visita.data_avvio,
-      data_scadenza: visita.data_scadenza
-    };
+    if(user){
+      var data = {
+        luogo: visita.luogo,
+        data_avvio: visita.data_avvio,
+        data_scadenza: visita.data_scadenza,
+        nomePaziente: visita.nomePaziente,
+        cognomePaziente: visita.cognomePaziente,
+        userid: user.id
+      };
+    }
+    
 
     VisitaDataService.create(data)
       .then(response => {
@@ -29,7 +42,10 @@ const AddVisita = () => {
           id: response.data.id,
           luogo: response.data.luogo,
           data_avvio: response.data.data_avvio,
-          data_scadenza: response.data.data_scadenza
+          data_scadenza: response.data.data_scadenza,
+          nomePaziente: response.data.nomePaziente,
+          cognomePaziente: response.data.cognomePaziente,
+          userid: response.data.userid
         });
         setSubmitted(true);
         console.log(response.data);
@@ -71,7 +87,7 @@ const AddVisita = () => {
           <div className="form-group">
             <label htmlFor="description">Data avvio</label>
             <input
-              type="text"
+              type="date"
               className="form-control"
               id="description"
               required
@@ -84,13 +100,39 @@ const AddVisita = () => {
           <div className="form-group">
             <label htmlFor="description">Data scadenza</label>
             <input
-              type="text"
+              type="date"
               className="form-control"
               id="data_scadenza"
               required
               value={visita.data_scadenza}
               onChange={handleInputChange}
               name="data_scadenza"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description">Nome paziente</label>
+            <input
+              type="text"
+              className="form-control"
+              id="nomePaziente"
+              required
+              value={visita.nomePaziente}
+              onChange={handleInputChange}
+              name="nomePaziente"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description">Cognome paziente</label>
+            <input
+              type="text"
+              className="form-control"
+              id="cognomePaziente"
+              required
+              value={visita.cognomePaziente}
+              onChange={handleInputChange}
+              name="cognomePaziente"
             />
           </div>
 
